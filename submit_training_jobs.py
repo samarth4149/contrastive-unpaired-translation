0c47b9c9b6c6c7b8b28cc21b84967a2b20e5892a
Samarth Mishra <samarth4149@gmail.com>
2023-11-10 13:22:43 -0500

    checking training for office home

--- submit_training_jobs.py
+++ submit_training_jobs.py
@@ -64,9 +64,9 @@ if __name__ == '__main__':
                 curr_opts.extend(['--save_latest_freq', '5000'])
                 curr_opts = TrainOptions(cmd_line=' '.join(curr_opts)).parse()
                 
-                # epoch = util.find_latest_checkpoint_epoch(curr_opts)
-                # print('Latest epoch :', epoch)
-                # continue
+                epoch = util.find_latest_checkpoint_epoch(curr_opts)
+                print('Latest epoch :', epoch)
+                continue
                 # if epoch is None:
                 #     raise Exception("No checkpoint found")
                 # curr_opts.continue_train = True
